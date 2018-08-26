df7677bfa46760d7b7841fdf5d8bdb7a6ba0a323
Juan Manuel Oubiña <juan.manuel.oubina@litebyte.us>
2018-08-26 19:30:38 -0300

    Add comment quantity

--- triggers.sql
+++ triggers.sql
@@ -5,9 +5,9 @@ CREATE TRIGGER after_comment_vote_insert
 	FOR EACH ROW
 	BEGIN
 		IF NEW.positive = TRUE THEN
-			UPDATE comments SET votes = votes + 1 WHERE comment_id = NEW.comment_id;
+			UPDATE comments SET votes = votes + 1 WHERE id = NEW.comment_id;
 		ELSE
-      UPDATE comments SET votes = votes - 1 WHERE comment_id = NEW.comment_id;
+      UPDATE comments SET votes = votes - 1 WHERE id = NEW.comment_id;
 		END IF;
 	END ;$$
 
@@ -17,9 +17,9 @@ CREATE TRIGGER after_comment_vote_edit
 	FOR EACH ROW
 	BEGIN
 		IF NEW.positive = TRUE THEN
-			UPDATE comments SET votes = votes + 1 WHERE comment_id = NEW.comment_id;
+			UPDATE comments SET votes = votes + 1 WHERE id = NEW.comment_id;
 		ELSE
-      UPDATE comments SET votes = votes - 1 WHERE comment_id = NEW.comment_id;
+      UPDATE comments SET votes = votes - 1 WHERE id = NEW.comment_id;
 		END IF;
 	END ;$$
 
@@ -29,9 +29,9 @@ CREATE TRIGGER after_comment_vote_delete
 	FOR EACH ROW
 	BEGIN
 		IF OLD.positive = TRUE THEN
-			UPDATE comments SET votes = votes - 1 WHERE comment_id = OLD.comment_id;
+			UPDATE comments SET votes = votes - 1 WHERE id = OLD.comment_id;
 		ELSE
-      UPDATE comments SET votes = votes + 1 WHERE comment_id = OLD.comment_id;
+      UPDATE comments SET votes = votes + 1 WHERE id = OLD.comment_id;
 		END IF;
 	END ;$$
 
@@ -41,9 +41,9 @@ CREATE TRIGGER after_post_vote_insert
 	FOR EACH ROW
 	BEGIN
 		IF NEW.positive = TRUE THEN
-			UPDATE posts SET votes = votes + 1 WHERE post_id = NEW.post_id;
+			UPDATE posts SET votes = votes + 1 WHERE id = NEW.post_id;
 		ELSE
-      UPDATE posts SET votes = votes - 1 WHERE post_id = NEW.post_id;
+      UPDATE posts SET votes = votes - 1 WHERE id = NEW.post_id;
 		END IF;
 	END ;$$
 
@@ -53,9 +53,9 @@ CREATE TRIGGER after_post_vote_edit
 	FOR EACH ROW
 	BEGIN
 		IF NEW.positive = TRUE THEN
-			UPDATE posts SET votes = votes + 1 WHERE post_id = NEW.post_id;
+			UPDATE posts SET votes = votes + 1 WHERE id = NEW.post_id;
 		ELSE
-      UPDATE posts SET votes = votes - 1 WHERE post_id = NEW.post_id;
+      UPDATE posts SET votes = votes - 1 WHERE id = NEW.post_id;
 		END IF;
 	END ;$$
 
@@ -65,30 +65,10 @@ CREATE TRIGGER after_post_vote_delete
 	FOR EACH ROW
 	BEGIN
 		IF OLD.positive = TRUE THEN
-			UPDATE posts SET votes = votes - 1 WHERE post_id = OLD.post_id;
+			UPDATE posts SET votes = votes - 1 WHERE id = OLD.post_id;
 		ELSE
-      UPDATE posts SET votes = votes + 1 WHERE post_id = OLD.post_id;
+      UPDATE posts SET votes = votes + 1 WHERE id = OLD.post_id;
 		END IF;
 	END ;$$
 
-DROP TRIGGER IF EXISTS after_comment_add;$$
-CREATE TRIGGER after_comment_add
-	AFTER INSERT ON comments
-	FOR EACH ROW
-	BEGIN
-		UPDATE posts set comment_quantity = comment_quantity + 1 WHERE id = NEW.post_id;
-        IF NEW.father != 0 THEN
-			UPDATE comments set comment_quantity = comment_quantity + 1 WHERE id = NEW.father;
-        END IF;
-	END ;$$
 
-DROP TRIGGER IF EXISTS after_comment_delete;$$
-CREATE TRIGGER after_comment_delete
-	AFTER DELETE ON comments
-	FOR EACH ROW
-	BEGIN
-		UPDATE posts set comment_quantity = comment_quantity - 1 WHERE id = OLD.post_id;
-        IF OLD.father != 0 THEN
-			UPDATE comments set comment_quantity = comment_quantity - 1 WHERE id = OLD.father;
-        END IF;
-	END ;$$
